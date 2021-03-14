40ec2bc0284d0617dff7aa82a21d0ca26c99ab97
Celia Ye <celiayehui@gmail.com>
2021-03-14 18:46:22 -0500

    added todo list in java

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
+    let db = firebase.firestore()
+
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
+
+    document.querySelector('form').addEventListener('submit', async function(event){
+      event.preventDefault()
+
+      let todoText = document.querySelector('#todo').value
+
+      if (todoText.length > 0){
+        // Add user ID to newly created to-do
+        let docRef = await db.collection('todos').add({
+          text: todoText,
+          userId: user.uid
+        })
+
+        let todoId = docRef.id
+        console.log(`new language to learn with ID ${todoId} created`)
+
+        document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+          <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+            ${todoText}
+          </div>
+        `)
+
+
+      }
+    })
+
+    //show only my to-dos
+    // let querySnapshot = await db.collection('todos').where('userId','==', user.uid).get()
+    // console.log(`Number to todos in collection`)
+
+    let response = await fetch(`/.netlify/functions/get_todos?userId=${user.uid}`)
+    let todos = await response.json()
+
+    for (let i = 0; i < todos.length; i++){
+      let todo = todos[i]
+      let todoId = todo.id
+      let todoText = todo.text
+    }
+
+    //create a sign-out button
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+    <button class = "text-pink-500 underline sign-out">Sign Out</button>
+    `
+
+    document.querySelector('.sign-out').addEventListener('click', function (event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'todo.html'
+    })
+
     console.log('signed in')
   } else {
     // Signed out
     console.log('signed out')
+    //hide the form when signed-out
+    document.querySelector('form').classList.add('hidden')
 
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
