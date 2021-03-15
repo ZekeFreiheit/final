d0b2a9a90d55930dbd05a40bbfb2f73ed710ea75
Celia Ye <celiayehui@gmail.com>
2021-03-14 20:02:43 -0500

    Read data from firebase

--- index.js
+++ index.js
@@ -23,29 +23,47 @@ firebase.auth().onAuthStateChanged(async function(user) {
         let todoId = docRef.id
         console.log(`new language to learn with ID ${todoId} created`)
 
-        document.querySelector('.todos').insertAdjacentHTML('beforeend', `
-          <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
-            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
-            ${todoText}
-          </div>
-        `)
+        // document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+        //   <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+        //     <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+        //     ${todoText}
+        //   </div>
+        // `)
 
 
       }
     })
 
     //show only my to-dos
-    // let querySnapshot = await db.collection('todos').where('userId','==', user.uid).get()
-    // console.log(`Number to todos in collection`)
+    let querySnapshot = await db.collection('todos').where('userId','==', user.uid).get()
+    console.log(`Number to todos in collection: ${querySnapshot.size}`)
 
-    let response = await fetch(`/.netlify/functions/get_todos?userId=${user.uid}`)
-    let todos = await response.json()
-
+    // let response = await fetch(`/.netlify/functions/get_todos?userId=${user.uid}`)
+    // let todos = await response.json()
+   
+    let todos = querySnapshot.docs
     for (let i = 0; i < todos.length; i++){
-      let todo = todos[i]
-      let todoId = todo.id
+      let todoId = todos[i].id
+      let todo = todos[i].data()
       let todoText = todo.text
+
+       //show todos
+    document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+    <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+      <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+      ${todoText}
+    </div>
+  `)
+    // eliminate done todos
+    document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
+      event.preventDefault()
+      document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
+      await db.collection('todos').doc(todoId).delete()
+    })
+
     }
+
+   
 
     //create a sign-out button
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
