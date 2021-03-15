a59964d67da2f5fd03509f92945fdf0dfae4e373
Celia Ye <celiayehui@gmail.com>
2021-03-14 19:05:38 -0500

    update gettodos

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,13 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {  apiKey: "AIzaSyCscvXxRwy0OqjqSKsGq5hYlYn4FLVypac",
+authDomain: "final-bespoken.firebaseapp.com",
+projectId: "final-bespoken",
+storageBucket: "final-bespoken.appspot.com",
+messagingSenderId: "483312461357",
+appId: "1:483312461357:web:0b5bc984585beb167cfaa4"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
---
+++ netlify/functions/get_todos.js
@@ -0,0 +1,27 @@
+let firebase = require('./firebase')
+
+exports.handler = async function(event) {
+  let queryStringUserId = event.queryStringParameters.userId
+  console.log(event);
+  let todosData = []
+
+  let db = firebase.firestore()
+
+  let querySnapshot = await db.collection('todos')
+    .where('userId', '==', queryStringUserId)
+    .get()
+  let todos = querySnapshot.docs
+  for (let i = 0; i < todos.length; i++) {
+    let todoId = todos[i].id
+    let todo = todos[i].data()
+    todosData.push({
+      id: todoId,
+      text: todo.text
+    })
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(todosData)
+  }
+}
